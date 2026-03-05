d9087596f9b70cc4f4e2f7963c221a86ca010ca1
oddava <javlonbekdeveloper@gmail.com>
2026-03-06 01:32:35 +0500

    fix: switch to vercel adapter, add prerender to blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   integrations: [react(), keystatic(), mdx()],
   trailingSlash: 'never',
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: vercel(),
   site: 'https://oddava.me',
 });
