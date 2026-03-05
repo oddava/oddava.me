e26399d7a061ed0bc43634c1d7d5cd98d97178c4
Oddava <oddava@proton.me>
2026-03-06 02:21:23 +0500

    fix: rewrite middleware to fix all keystatic API routes on vercel

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,16 @@
 import { defineMiddleware } from 'astro:middleware';
 
 export const onRequest = defineMiddleware((context, next) => {
-    if (
-        context.url.pathname.includes('/github/oauth/') ||
-        context.url.pathname.includes('/github/login')
-    ) {
+    if (context.url.pathname.startsWith('/api/keystatic')) {
         const forwardedHost = context.request.headers.get('x-forwarded-host');
-        const forwardedProto = context.request.headers.get('x-forwarded-proto') || 'https';
+        const forwardedProto =
+            context.request.headers.get('x-forwarded-proto') || 'https';
 
         if (forwardedHost) {
-            const newUrl = new URL(context.url);
-            newUrl.hostname = forwardedHost;
-            newUrl.protocol = forwardedProto;
-            newUrl.port = '';
+            const newUrl = new URL(context.url.pathname + context.url.search, `${forwardedProto}://${forwardedHost}`);
 
             return next(
-                new Request(newUrl.toString(), {
-                    method: context.request.method,
-                    headers: context.request.headers,
-                    body: context.request.body,
-                })
+                new Request(newUrl.toString(), context.request)
             );
         }
     }
