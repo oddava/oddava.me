b1b67aac13d422464c93e090037685050f803698
oddava <javlonbekdeveloper@gmail.com>
2026-03-13 06:02:26 +0500

    feat: Add projects content collection, Keystatic integration, and dedicated display pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,17 @@ import keystatic from '@keystatic/astro';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  integrations: [react({ experimental: { jsxImportSource: true } }), keystatic(), mdx()],
+  integrations: [react(), keystatic(), mdx()],
   trailingSlash: 'never',
   output: 'server',
   adapter: vercel(),
   site: 'https://oddava.me',
+  vite: {
+    resolve: {
+      dedupe: ['yjs', 'react', 'react-dom'],
+    },
+    optimizeDeps: {
+      include: ['yjs'],
+    },
+  },
 });
