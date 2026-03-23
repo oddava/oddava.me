1c87d1743cf2186cdc428bb987b9ae32cf26ce03
Oddava <oddava@proton.me>
2026-03-23 15:04:44 +0500

    refactor: update Keystatic integration to use internal Astro page and virtual config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,17 @@ export default defineConfig({
   adapter: vercel(),
   site: 'https://oddava.me',
   vite: {
+    plugins: [
+      {
+        name: 'keystatic-virtual-config',
+        resolveId(id) {
+          if (id === 'virtual:keystatic-config') {
+            return this.resolve('./keystatic.config', './a');
+          }
+          return null;
+        },
+      },
+    ],
     resolve: {
       dedupe: ['yjs', 'react', 'react-dom'],
     },
--- src/components/keystatic/KeystaticPage.tsx
+++
@@ -1,6 +0,0 @@
-import { makePage } from '@keystatic/astro/ui';
-import config from '../../../keystatic.config';
-
-const KeystaticPage = makePage(config);
-
-export default KeystaticPage;
